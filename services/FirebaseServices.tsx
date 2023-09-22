4d0e875e9bf4714fc394791da3707c9e75e577dc
Samson Achiaga <sirhumble84@gmail.com>
2023-09-22 20:44:44 +0100

    @init/Firebase init

---
+++ services/FirebaseServices.tsx
@@ -0,0 +1,22 @@
+import { Auth, createUserWithEmailAndPassword, GoogleAuthProvider } from "firebase/auth";
+import firebase, { initializeApp } from "firebase/app";
+
+export const signup = (auth, email, password) => {
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
+  }
+
+//   export const signInWithGoogle = (auth: Auth, provider) => {
+//     firebase.auth
+//     const googleProvider = new GoogleAuthProvider();
+//     auth.signInWithPopup(googleProvider)
+//   }
