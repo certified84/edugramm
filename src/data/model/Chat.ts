693e3826341a5d4c5264448496d338b14fb09f39
Samson Achiaga <sirhumble84@gmail.com>
2023-11-08 02:25:50 +0100

    @update/Update chat feature - Implement send and receive message

--- src/data/model/Chat.ts
+++ src/data/model/Chat.ts
@@ -1,15 +1,25 @@
+import { FieldValue, serverTimestamp } from "firebase/firestore"
+
 export type Chat = {
-    id: string,
-    senderId: string,
-    message: string,
-    date?: number | null,
+    _id: string,
+    text: string,
+    createdAt: Date,
     read: boolean,
+    user: {
+        _id: string,
+        name: string,
+        avatar?: string | null,
+    }
 }
 
 export const defaultChat: Chat = {
-    id: "",
-    senderId: "",
-    message: "",
-    date: null,
+    _id: `${new Date().getTime()}`,
+    text: "",
+    createdAt: new Date(),
     read: false,
+    user: {
+        _id: "",
+        name: "",
+        avatar: "",
+    }
 }
--- src/screens/global/Chat.tsx
+++ src/screens/global/Chat.tsx
@@ -1,19 +1,19 @@
 import "expo-dev-client";
 import { useNavigation } from "@react-navigation/native";
-import { SafeAreaView, TouchableOpacity, View, Text, StyleSheet, Platform, StatusBar, Image } from "react-native";
+import { SafeAreaView, TouchableOpacity, View, Text, StyleSheet, Platform, StatusBar, Image, KeyboardAvoidingView } from "react-native";
 import { MaterialIcons, Ionicons, AntDesign, Feather } from '@expo/vector-icons'
 import { COLORS, SIZES, TYPOGRAPHY } from "../../../assets/theme";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import VerifiedIcon from "../../components/VerifiedIcon";
 import { Avatar, TextInput } from 'react-native-paper';
 import { SplashIcon } from "../../../assets/svg/SplashIcon";
 import AgoraUIKit from 'agora-rn-uikit';
 import { auth, firestore } from "../../../firebase";
-import { Timestamp, addDoc, collection, doc, updateDoc } from "firebase/firestore";
+import { Timestamp, addDoc, collection, doc, orderBy, query, updateDoc } from "firebase/firestore";
 import { useCollection, useDocument } from "react-firebase-hooks/firestore";
 import { defaultUser } from "../../data/model/User";
 import { defaultChat, Chat } from "../../data/model/Chat";
-import { FlatList } from "react-native-gesture-handler";
+import { GiftedChat, Bubble, Actions, Send, InputToolbar } from 'react-native-gifted-chat'
 
 export default function ChatScreen({ route }) {
 
@@ -43,11 +43,17 @@ export default function ChatScreen({ route }) {
     const [chatSnapshot2, chatLoading2, chatError2] = useCollection(chatRef2)
 
     const mainChatRef = collection(firestore, `chats/${values.chatId}/messages`)
-    const [mainChatSnapshot, mainChatLoading, mainChatError] = useCollection(mainChatRef)
+    const q = query(mainChatRef, orderBy("createdAt", "desc"))
+    const [mainChatSnapshot, mainChatLoading, mainChatError] = useCollection(q)
 
     useEffect(() => {
         if (mainChatSnapshot) {
-            const data = mainChatSnapshot.docs
+            const data = mainChatSnapshot.docs.map((doc) => ({
+                _id: doc.data()._id,
+                createdAt: doc.data().createdAt.toDate(),
+                text: doc.data().text,
+                user: doc.data().user,
+            }))
             setValues({
                 ...values,
                 messages: data
@@ -67,7 +73,8 @@ export default function ChatScreen({ route }) {
                 chatId: `${user.uid}_${values.userInfo.uid}`
             })
         }
-    }, [chatSnapshot])
+        console.log("Chat ID: ", values.chatId)
+    }, [chatSnapshot, chatSnapshot2])
 
     useEffect(() => {
         if (snapshot && snapshot.exists()) {
@@ -87,19 +94,33 @@ export default function ChatScreen({ route }) {
         }
     }, [error])
 
-    async function sendMessage() {
+    const onSend = useCallback((messages = []) => {
+        setValues(previousState => ({ ...values, messages: GiftedChat.append(previousState.messages, messages) }))
+        const { text } = messages[0]
+        sendMessage(text)
+        // saveLastConversation(text, createdAt) // Recent chat
+        // sendDeviceNotification(text)
+
+    }, [])
+
+    async function sendMessage(text) {
         const data: Chat = {
             ...defaultChat,
-            senderId: user.uid,
-            message: values.message,
-            date: Timestamp.now().toMillis(),
+            text: text,
+            createdAt: new Date(),
+            user: {
+                _id: user.uid,
+                name: user.displayName,
+                avatar: user.photoURL
+            }
         }
-
-        const docRef = addDoc(mainChatRef, data)
-        await docRef.then((snapshot) => {
-            setValues({ ...values, message: "" })
-            updateDoc(snapshot, { id: snapshot.id })
-        })
+        const chatId = values.chatId
+        console.log("Chat ID: ", chatId)
+        await addDoc(collection(firestore, `chats/${chatId}/messages`), data)
+            .then((snapshot) => {
+                setValues({ ...values, message: "" })
+                updateDoc(snapshot, { _id: snapshot.id })
+            })
             .catch((error) => {
                 const errorCode = error.code
                 const errorMessage = error.message
@@ -127,33 +148,9 @@ export default function ChatScreen({ route }) {
                         <TouchableOpacity onPress={() => { navigation.goBack() }}>
                             <Ionicons name="chevron-back" size={SIZES.xl} color={COLORS.onSurface} />
                         </TouchableOpacity>
-                        {/* <View style={{ width: SIZES.sm }} /> */}
-                        {/* <TouchableOpacity style={{ flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start' }}>
-                            <View style={{ marginEnd: SIZES.sm, overflow: 'hidden', width: 43, height: 43, borderRadius: 43 / 2, backgroundColor: COLORS.primary, justifyContent: 'center', alignItems: 'center' }}>
-                                {userInfo.photo ?
-                                    <Avatar.Image size={40} source={{ uri: userInfo.photo }} />
-                                    : <SplashIcon />
-                                }
-                            </View>
-                            <View>
-                                <View style={{ flexDirection: 'row' }}>
-                                    <Text style={{ ...TYPOGRAPHY.h2 }} numberOfLines={1}>{userInfo.name}</Text>
-                                    {userInfo.verified && <VerifiedIcon />}
-                                </View>
-                                <Text style={{ ...TYPOGRAPHY.h2, fontSize: SIZES.xs, color: COLORS.onSurface, opacity: .8 }}>Active now</Text>
-                            </View>
-                        </TouchableOpacity> */}
                     </View>
                 )
             },
-            // headerTransparent: true,
-            // headerTItle: () => {
-            //     return (
-            //         <View style={{flex: 1, backgroundColor: 'purple'}}>
-            //             <Text>Samson Achiaga</Text>
-            //         </View>
-            //     )
-            // },
             headerRight: () => {
                 return (
                     <View style={{ flexDirection: 'row', alignItems: 'center' }}>
@@ -168,6 +165,47 @@ export default function ChatScreen({ route }) {
             }
         })
     }, [])
+
+    const renderSend = (props) => {
+        return (
+            <Send {...props}>
+                <View style={{ padding: SIZES.xxs + 4, borderRadius: 100, alignItems: 'center', marginHorizontal: SIZES.xxs, backgroundColor: COLORS.onSecondary }}>
+                    <Feather name='send' size={SIZES.md} color={COLORS.onSurface} />
+                </View>
+            </Send>
+        )
+    }
+
+    const renderBubble = (props) => {
+        return (
+            <Bubble
+                {...props}
+                textStyle={{
+                    right: {
+                        color: COLORS.onPrimaryContainer,
+                    },
+                    left: {
+                        color: COLORS.onSecondaryContainer,
+                    }
+                }}
+                wrapperStyle={{
+                    right: {
+                        color: COLORS.white,
+                        backgroundColor: COLORS.primaryContainer
+                    },
+                    left: {
+                        color: COLORS.white,
+                        backgroundColor: COLORS.secondaryContainer
+                    }
+                }}
+            />
+        )
+    }
+
+    const renderInputToolbar = (props) => {
+        //Add the extra styles via containerStyle
+       return <InputToolbar {...props} containerStyle={{borderTopWidth: 1.5, borderTopColor: '#333', backgroundColor: COLORS.surface}} />
+     }
 
     return (
         videoCall ? <AgoraUIKit connectionData={connectionData} rtcCallbacks={callbacks} /> :
@@ -204,18 +242,23 @@ export default function ChatScreen({ route }) {
 
                 <View style={{ height: 1, backgroundColor: COLORS.white, opacity: .2, marginTop: SIZES.xxs }} />
 
-                <View style={{ flex: 1}}>
+                <View style={{ flex: 1 }}>
 
-                    <FlatList
-                        // style={{ zIndex: 2 }}
-                        data={values.messages}
-                        // ListHeaderComponent={() => <HomeHeader titleText={"EduGramm"} navigation={navigation} userInfo={values} />}
-                        renderItem={({ item }) => <Text style={{...TYPOGRAPHY.h2, color: 'white', marginTop: SIZES.md}}>{item.data().message}</Text>}
-                        keyExtractor={(item) => item.id}
-                        alwaysBounceVertical={values.messages.length > 0}
+                    <GiftedChat
+                        messages={values.messages}
+                        renderSend={renderSend}
+                        renderBubble={renderBubble}
+                        renderInputToolbar={renderInputToolbar}
+                        // alwaysShowSend
+                        // messagesContainerStyle={{ backgroundColor: 'green'}}
+                        textInputStyle={{ color: COLORS.onSurface }}
+                        onSend={messages => onSend(messages)}
+                        user={{ _id: user.uid }}
+                        scrollToBottom
                     />
+                    {Platform.OS === 'android' && <KeyboardAvoidingView behavior="padding" />}
 
-                    <View style={{
+                    {/* <View style={{
                         // flex: 1,
                         flexDirection: 'row',
                         // width: '100%',
@@ -225,8 +268,8 @@ export default function ChatScreen({ route }) {
                         alignItems: 'flex-end',
                         marginHorizontal: SIZES.xs,
                         borderRadius: 25
-                    }}>
-                        <TextInput
+                    }}> */}
+                    {/* <TextInput
                             mode="outlined"
                             placeholder='Message'
                             value={values.message}
@@ -241,25 +284,9 @@ export default function ChatScreen({ route }) {
                             activeOutlineColor={COLORS.secondaryContainer}
                             placeholderTextColor={COLORS.onSecondaryContainer}
                             textColor={COLORS.onSecondaryContainer}
-                        />
-                        <TouchableOpacity
-                            activeOpacity={.6}
-                            style={{ padding: SIZES.xxs + 4, position: 'absolute', right: 4, bottom: SIZES.xxs / 2, borderRadius: 100, alignItems: 'center', marginHorizontal: SIZES.xxs, backgroundColor: COLORS.onSecondary, opacity: values.message.length > 0 ? 1 : .5, marginBottom: SIZES.xxs }}
-                            disabled={values.message.length < 1}
-                            onPress={() => values.message.length > 0 ? sendMessage() : {}}
-                        >
-                            <Feather name='send' size={SIZES.md} color={COLORS.onSurface} />
-                            {/* <Text style={{
-                        ...TYPOGRAPHY.h2,
-                        color: COLORS.primary,
-                        // position: "absolute",
-                        right: SIZES.md,
-                        // bottom: -40,
-                        // bottom: 15,
-                        fontSize: SIZES.md
-                    }}>Send</Text> */}
-                        </TouchableOpacity>
-                    </View>
+                        /> */}
+
+                    {/* </View> */}
                 </View>
             </SafeAreaView>
     )
